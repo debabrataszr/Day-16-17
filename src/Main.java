a1cbc6aade8e434808dfa8d26987323e4ca31cfd
debabrata-97 <debabrataszr@gmail.com>
2023-03-19 15:15:28 +0530

    An Anagram Detection Example

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,31 @@
+import java.util.Arrays;
+
 public class Main {
 
-    /* Function to merge the subarrays of a[] */
-    void merge(String a[], int beg, int mid, int end) {
-        int i, j, k;
-        int n1 = mid - beg + 1;
-        int n2 = end - mid;
-
-        /* temporary Arrays */
-        String LeftArray[] = new String[n1];
-        String RightArray[] = new String[n2];
-
-        /* copy data to temp arrays */
-        for (i = 0; i < n1; i++)
-            LeftArray[i] = a[beg + i];
-        for (j = 0; j < n2; j++)
-            RightArray[j] = a[mid + 1 + j];
-
-        i = 0; /* initial index of first sub-array */
-        j = 0; /* initial index of second sub-array */
-        k = beg;  /* initial index of merged sub-array */
-
-        while (i < n1 && j < n2) {
-            if (LeftArray[i].compareTo(RightArray[i]) <= 0) {
-                a[k] = LeftArray[i];
-                i++;
-            } else {
-                a[k] = RightArray[j];
-                j++;
-            }
-            k++;
-        }
-        while (i < n1) {
-            a[k] = LeftArray[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            a[k] = RightArray[j];
-            j++;
-            k++;
-        }
-    }
-
-    void mergeSort(String a[], int beg, int end) {
-        if (beg < end) {
-            int mid = (beg + end) / 2;
-            mergeSort(a, beg, mid);
-            mergeSort(a, mid + 1, end);
-            merge(a, beg, mid, end);
-        }
-    }
-
-    /* Function to print the array */
-    void printArray(String a[], int n) {
-        int i;
-        for (i = 0; i < n; i++)
-            System.out.print(a[i] + " ");
-    }
-
     public static void main(String[] args) {
-        String a[] = {"Grapes", "Cucumber", "Banana", "Apple"};
-        int n = a.length;
-        Main m1 = new Main();
-        System.out.println("\nBefore sorting array elements are - ");
-        m1.printArray(a, n);
-        m1.mergeSort(a, 0, n - 1);
-        System.out.println("\nAfter sorting array elements are - ");
-        m1.printArray(a, n);
-        System.out.println(" ");
+        boolean result = isAnagram("abcd","dcba");
+        System.out.println(result);
+    }
+    public static boolean isAnagram(String first, String second)
+    {
+        // remove all whitespaces and convert strings to lowercase
+        first  = first.replaceAll("\\s", "").toLowerCase();
+        second = second.replaceAll("\\s", "").toLowerCase();
+
+        /* check whether string lengths are equal or not,
+        if unequal then not anagram */
+        if (first.length() != second.length())
+            return false;
+
+        // convert string to char array
+        char[] firstArray = first.toCharArray();
+        char[] secondArray = second.toCharArray();
+
+        // sort both the arrays
+        Arrays.sort(firstArray);
+        Arrays.sort(secondArray);
+
+        // checking whether both strings are equal or not
+        return Arrays.equals(firstArray,secondArray);
     }
 }
