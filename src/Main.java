6129ffc2114523f9928b357fea8f70d5a953c36e
debabrata-97 <debabrataszr@gmail.com>
2023-03-19 12:42:21 +0530

    Bubble Sort

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,56 @@ public class Main {
     public static void main(String[] args) {
         String word = "abc";
         Main main = new Main();
+        //   main.binarySearch();
         main.insertionSort();
+        main.bubbleSort();
+    }
+
+
+    public void binarySearch() {
+        String[] array = {"India", "is", "my", "country"};
+        stringBinary(array, "India");
+    }
+
+    public static void stringBinary(String[] array, String search) {
+        int mid;
+        int min = 0, max = array.length - 1;
+        while (min <= max) {
+            mid = (min + max) / 2;
+            if (array[mid].compareTo(search) < 0) {
+                min = mid + 1;
+            } else if (array[mid].compareTo(search) > 0) {
+                max = mid - 1;
+            } else if (array[mid].compareTo(search) == 0) {
+                System.out.println("Key found");
+                return;
+            }
+        }
+        System.out.println("key not found ");
     }
 
     public void insertionSort() {
         String[] array = {"my", "name", "is", "Debabrata"};
-        String max = "null";
+        for (int i = 0; i < array.length; i++) {
+            String word = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j].compareTo(word) > 0) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = word;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public void bubbleSort() {
+        int[] array = {1, 35, 63, 24, 85, 3, 9};
+        int max = 0;
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
-                if (array[i].compareToIgnoreCase(array[j]) > 0) {
+                if (array[i] > array[j]) {
                     max = array[i];
                     array[i] = array[j];
                     array[j] = max;
@@ -18,7 +59,7 @@ public class Main {
             }
         }
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + " ");
         }
     }
 }
