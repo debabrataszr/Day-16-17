d7dfef43230280dbb92614b8344965b7a1dac13f
debabrata-97 <debabrataszr@gmail.com>
2023-03-19 12:10:57 +0530

    Binary Search the Word from Word List

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         String word = "abc";
-        System.out.println("Iterative Method");
-        permutation(word);
-        System.out.println("\n\nRecursive Method");
-        solve(word.toCharArray(), 0);
+        binarySearch();
     }
 
-    public static void permutation(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            for (int j = 0; j < word.length(); j++) {
-                for (int k = 0; k < word.length(); k++) {
-                    if (word.charAt(i) != word.charAt(j) && word.charAt(j) != word.charAt(k)
-                            && word.charAt(k) != word.charAt(i))
+    public static void binarySearch() {
+        String[] array = {"we", "the", "people", "of", "India"};
+        stringBinary(array, "people");
+    }
 
-                        System.out.print((word.charAt(i) + "" + word.charAt(j) + "" + word.charAt(k)) + " ");
-                }
+    public static void stringBinary(String[] array, String search) {
+        int mid;
+        int min = 0, max = array.length - 1;
+        while (min <= max) {
+            mid = (min + max) / 2;
+            if (array[mid].compareTo(search) < 0) {
+                min = mid + 1;
+            } else if (array[mid].compareTo(search) > 0) {
+                max = mid - 1;
+            } else if (array[mid].compareTo(search) == 0) {
+                System.out.println("Key found");
+                return;
             }
         }
-    }
-
-    public static void swap(char[] arr, int index, int index2) {
-        char temp = arr[index];
-        arr[index] = arr[index2];
-        arr[index2] = temp;
-    }
-
-    public static void solve(char[] arr, int index) {
-        if (index == arr.length - 1) { //Base condition of recursion
-            System.out.print(String.valueOf(arr) + " ");
-        }
-
-        for (int i = index; i < arr.length; i++) {
-            swap(arr, index, i);
-            solve(arr, index + 1);
-            swap(arr, index, i);
-        }
+        System.out.println("Key not found ");
     }
 }
